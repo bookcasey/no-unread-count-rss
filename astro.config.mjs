0c48073f631a8eeb2d13f164c18e68e51c128a88
Casey Olson <casey@bookcasey.com>
2024-09-05 18:20:59 -0600

    update astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bookcasey.github.io/no-unread-count-rss',
+  base: 'no-unread-count-rss'
+});
